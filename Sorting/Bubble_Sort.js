ca5407bf6a0dd00b598001a6c93367ebb32aa64e
Yanek K <yanek.dev@gmail.com>
2021-05-01 00:26:58 -0400

    Bubble Sort

---
+++ Sorting/Bubble_Sort.js
@@ -0,0 +1,37 @@
+// Simple Version of Bubble Sort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+//Better version of Bubble Sort
+
+function bubbleSort2(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    // ** Optimization for almost sorted array ** //
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort2([1, 4, 6, 3, 2, 3, 5, 1]));
