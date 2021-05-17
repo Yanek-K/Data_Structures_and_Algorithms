c855a655eafa0cf5b13ec2e600e5ffacc54a9abd
Yanek K <yanek.dev@gmail.com>
2021-05-17 17:33:00 -0400

    SLL - SET

--- Data_Structures/Singly_Linked_List/index.js
+++ Data_Structures/Singly_Linked_List/index.js
@@ -87,6 +87,14 @@ class SinglyLinkedList {
     }
     return node;
   }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 list = new SinglyLinkedList();
