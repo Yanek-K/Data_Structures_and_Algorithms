4ca07a565e63568f9173773dfaef122543794de5
Yanek K <yanek.dev@gmail.com>
2021-05-27 12:04:30 -0400

    Binary Search Tree

---
+++ Data_Structures/Trees/index.js
@@ -0,0 +1,57 @@
+// ** TREES ** //
+
+// BIG O OF TREES //
+// Insertion O()
+// Removal O()
+// Searching O()
+// Access O()
+
+//  ** USES ** //
+// HTML + DOM
+// Network Routing
+// Abstract Syntax Trees
+// Artificial Intelligence
+// Folders in Operating Systems
+// JSON
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+
+var tree = new BST();
