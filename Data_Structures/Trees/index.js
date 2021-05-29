966e09387f76020e71f63e4837fcf5dabe4d5a1e
Yanek K <yanek.dev@gmail.com>
2021-05-29 17:55:46 -0400

    Tree - DFS - Post Order

--- Data_Structures/Trees/index.js
+++ Data_Structures/Trees/index.js
@@ -87,11 +87,22 @@ class BST {
     traverse(this.root);
     return data;
   }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 var tree = new BST();
 tree.insert(10);
-tree.insert(5);
+tree.insert(6);
 tree.insert(15);
-tree.insert(1);
-tree.insert(16);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
