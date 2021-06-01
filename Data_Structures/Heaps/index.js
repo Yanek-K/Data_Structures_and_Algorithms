ca2612685d45eb431649e74155a88e15759fe3dd
Yanek K <yanek.dev@gmail.com>
2021-06-01 13:44:09 -0400

    Heaps - Extract Max

--- Data_Structures/Heaps/index.js
+++ Data_Structures/Heaps/index.js
@@ -31,6 +31,49 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let parentIndex = 0;
+    let length = this.values.length;
+    let parentValue = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * parentIndex + 1;
+      let rightChildIndex = 2 * parentIndex + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > parentValue) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          rightChild > parentValue &&
+          rightChild > leftChild
+          // (swap === null && rightChild > parentValue) ||
+          // (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[parentIndex] = this.values[swap];
+      this.values[swap] = parentValue;
+      parentIndex = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -40,3 +83,4 @@ heap.insert(33);
 heap.insert(18);
 heap.insert(27);
 heap.insert(12);
+heap.insert(55);
